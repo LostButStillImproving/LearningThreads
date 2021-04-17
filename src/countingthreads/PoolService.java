38d7d6e96144f020505ffe9a9425a822b6a75758
LostButStillImproving <mike6353@edu.easj.dk>
2021-04-17 15:51:24 +0200

    refactored out a bunch

---
+++ src/countingthreads/PoolService.java
@@ -0,0 +1,32 @@
+package countingthreads;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class PoolService {
+
+    static void startPool(int amountOfThreads, int delay) {
+
+        ScheduledExecutorService pool = Executors.newScheduledThreadPool(3);
+        IntStream.rangeClosed(1, amountOfThreads).forEach(number -> pool.schedule(
+                Logger.getLogger(number), (long) number * delay, TimeUnit.MILLISECONDS));
+        pool.shutdown();
+    }
+
+    public static void main(String[] args) {
+        startPool(10,20);
+        startPool(10,20);
+        startPool(10,20);
+        startPool(10,20);
+        startPool(10,20);
+        startPool(10,20);
+        startPool(10,20);
+        startPool(10,20);
+        startPool(10,20);
+        startPool(10,20);
+        startPool(10,20);
+        startPool(10,20);
+    }
+}
