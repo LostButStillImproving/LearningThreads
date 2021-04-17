ce1d3f354aaa71f2937dfd17b91fb7369da4d14e
LostButStillImproving <mike6353@edu.easj.dk>
2021-04-17 16:34:20 +0200

    minor

--- src/countingthreads/PoolService.java
+++ src/countingthreads/PoolService.java
@@ -8,8 +8,7 @@ import java.util.stream.IntStream;
 
 public class PoolService {
 
-    public static ThreadSpawner.Callback startLoggerPool(int amountOfThreads, int delay, int maxWorkerThreads) {
-
+    public static Callback startLoggerPool(int amountOfThreads, int delay, int maxWorkerThreads) {
         ScheduledExecutorService pool = Executors.newScheduledThreadPool(maxWorkerThreads);
         IntStream.rangeClosed(1, amountOfThreads).forEach(number -> pool.schedule(
                 Logger.getLogger(number), (long) number * delay, TimeUnit.MILLISECONDS));
@@ -20,6 +19,5 @@ public class PoolService {
     public static void main(String[] args) {
         startLoggerPool(10,20,5);
         startLoggerPool(10,20,10);
-        ThreadSpawner.spawnThread(startLoggerPool(20,20,20));
     }
 }
--- src/countingthreads/ThreadSpawner.java
+++ src/countingthreads/ThreadSpawner.java
@@ -2,10 +2,16 @@ package countingthreads;
 
 public class ThreadSpawner {
 
-    public static void spawnThread(Callback callback) {
-        new Thread(callback::onCallBack).start();
+    private ThreadSpawner() {
     }
-    public interface Callback {
-        void onCallBack();
+
+    public static void spawnThread(Callback callback) throws InterruptedException {
+        var thread = new Thread(callback::onCallBack);
+        thread.join();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        spawnThread(PoolService.startLoggerPool(10,1000,10));
+        spawnThread(PoolService.startLoggerPool(10,1000,10));
     }
 }
